c22dfca45ff783e9a5fb746a8a396e26ea642330
Hmt <Hamiltonwu717@gmail.com>
2024-04-02 11:45:35 +0800

    V3333333

--- Program.cs
+++ Program.cs
@@ -2,4 +2,9 @@
 Console.WriteLine("Hello, World!");
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, aespa");
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, HIHIHI!");
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, PIPIPI");
+
 
